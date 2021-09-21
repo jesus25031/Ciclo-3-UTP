c434b87bd1ae98162d9e12b3978e0aae624cf8e0
jesus25031 <jesusviloria2503@gmail.com>
2021-09-20 23:14:31 -0500

    Sprint 2 lunes 20 Septiembre

---
+++ sprint2/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">>
+    <CrearCuenta msg="APPVENTARIO"/>
+  </div>
+</template>
+
+<script>
+import CrearCuenta from './components/CrearCuenta.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CrearCuenta
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sprint2/src/components/CrearCuenta.vue
@@ -0,0 +1,63 @@
+<template>
+    <main class="form-signin">
+        <div class="card">
+               <div class="card-body">
+                    <form>
+                        <h2 class="h3 mb-3 fw-normal text-center">CREAR CUENTA</h2>
+
+                        <div class="form-group">
+                            <label>Nombre</label>
+                            <input type="text" class="form-control form-control-lg" placeholder="Nombre"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Apellido</label>
+                            <input type="text" class="form-control form-control-lg" placeholder="Apellido"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control form-control-lg" placeholder="Email" />
+                        </div>
+
+                        <div class="form-group">
+                            <label>Contraseña</label>
+                            <input type="password" class="form-control form-control-lg" placeholder="Contraseña"/>
+                        </div>                        
+
+                        <router-link class="w-100 btn btn-lg btn-primary" type="submit" to="/signin">Crear Cuenta</router-link>
+                    </form>
+                </div>
+        </div>  
+        <p class="mt-3 mb-3 text-muted text-center">© Appventario 2021</p>
+    </main>      
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        methods: {
+            
+        }
+    }
+</script>
+
+<style lang="css">
+    body {
+        display: flex;
+        padding-top: 60px;
+        padding-bottom: 60px;        
+        align-items: center;
+        background-color: #f6f6f6;
+    }
+    .form-signin {
+        width: 100%;
+        max-width: 450px;
+        margin: auto;
+    }
+    label {
+        font-weight: 600;
+    }
+</style>
